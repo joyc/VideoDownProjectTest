3429eb08263924ff86e289a03eb5b32c409200ac
joyc <joyc@live.jp>
2017-03-08 01:19:48 +0900

    Initial commit

---
+++ video_title.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+request = requests.get("https://www.youtube.com/results?search_query=pitbull")
+content = request.content
+soup = BeautifulSoup(content, "html.parser")
+for element in soup.find_all('a', {"rel": "spf-prefetch"}):
+    # print(element)
+    video_title = element.get('title')
+    print(video_title)
